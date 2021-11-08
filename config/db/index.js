d9140e916ce1a932f7c30725ae496312c296cdb2
iL3sor <92763293+iL3sor@users.noreply.github.com>
2021-11-08 16:35:46 +0700

    Update index.js

--- config/db/index.js
+++ config/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 async function connect() {
   try {
-    await mongoose.connect("mongodb+srv://il3sor:0823572165@cluster0.6txrx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" , {
+    await mongoose.connect("mongodb+srv://il3sor:0823572165@cluster0.6txrx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" ,{
     });
     console.log("success");
   } catch (error) {
